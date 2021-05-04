196840c4526a4f6d47e491eb858a643114598df8
Fernando Colom <colomfernando@gmail.com>
2021-05-04 12:31:47 -0300

    install and setup styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React, { ReactElement } from 'react';
+import GlobalStyles from './GlobalStyles';
 
 const App: React.FC = (): ReactElement => (
-  <div className="App">
-    <header className="App-header">
-      <p>covid</p>
-    </header>
-  </div>
+  <>
+    <GlobalStyles />
+    <div className="App">
+      <header className="App-header">
+        <p>covid</p>
+      </header>
+    </div>
+  </>
 );
 
 export default App;
