3a2880715fa71a8ef740d79935f728f8c7bd8aad
Fernando Colom <colomfernando@gmail.com>
2021-05-04 13:22:33 -0300

    add palette

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import GlobalStyles from './GlobalStyles';
+import GlobalStyles from 'theme/GlobalStyles';
 
 const App: React.FC = (): JSX.Element => (
   <>
---
+++ src/theme/palette.js
@@ -0,0 +1,36 @@
+const palette = {
+  primary: {
+    main: '#4ce0b3',
+    dark: '#36ad89',
+    contrastText: '#fff',
+  },
+  secondary: {
+    main: '#ff8e72',
+    dark: '#db775e',
+    contrastText: '#fff',
+  },
+  error: {
+    main: '#ff3333',
+  },
+  success: {
+    main: '#4cc355',
+  },
+  common: {
+    black: '#000',
+    white: '#fff',
+  },
+  grey: {
+    50: '#fafafa',
+    100: '#f5f5f5',
+    200: '#eeeeee',
+    300: '#e0e0e0',
+    400: '#bdbdbd',
+    500: '#9e9e9e',
+    700: '#616161',
+    600: '#757575',
+    800: '#424242',
+    900: '#212121',
+  },
+};
+
+export default palette;
